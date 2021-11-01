abd145c881431c189412c9b94620b815f728fd38
anderkazuhiro <anderkazuhiro@hotmail.com>
2021-11-01 10:02:56 -0300

    Delete avaliacao/src/main/java/com/lumini/avaliacao/Service directory

--- avaliacao/src/main/java/com/lumini/avaliacao/Service/UtilService.java
+++
@@ -1,8 +0,0 @@
-package com.lumini.avaliacao.Service;
-
-import com.lumini.avaliacao.model.CompanyAddress;
-
-public interface UtilService {
-
-    CompanyAddress getAddress(String companyDocument);
-}
